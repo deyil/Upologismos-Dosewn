bad12602eff81643a9b9854e7ab63f24a08b461b
Demetris <mr.deyil@gmail.com>
2011-12-15 14:10:53 +0200

    test

---
+++ Upologistis_Dosewn/Upologistis_Dosewn/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Upologistis_Dosewn
+//
+//  Created by Demetris Boukos on 15/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+    int test;
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
