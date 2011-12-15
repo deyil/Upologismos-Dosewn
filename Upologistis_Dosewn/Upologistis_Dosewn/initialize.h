f93e082d04b281c3a6cc99ba6e04615aad808e39
Demetris <mr.deyil@gmail.com>
2011-12-15 16:46:00 +0200

    header implement

---
+++ Upologistis_Dosewn/Upologistis_Dosewn/initialize.h
@@ -0,0 +1,34 @@
+//
+//  initialize.h
+//  Upologistis_Dosewn
+//
+//  Created by Demetris Boukos on 15/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+#ifndef Upologistis_Dosewn_initialize_h
+#define Upologistis_Dosewn_initialize_h
+
+#include <iostream>
+using namespace std;
+
+int y = 1;
+
+float Kef_arx;
+
+float Tokos;
+
+int years;
+
+float dosi_statheri;
+
+float Kef_meiwmeno;
+
+float epitokio();
+
+float dosi();
+
+void calculation();
+
+void input();
+
+#endif
